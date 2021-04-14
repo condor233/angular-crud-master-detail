5de00cbf3dd336d72267e89bcbeb6abe96f4294a
condor233 <e.hdelemos@gmail.com>
2021-04-14 00:27:06 -0300

    adiciona metodos basicos em CategoryForm

--- financeiro/src/app/app.module.ts
+++ financeiro/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CategoriesModule } from './pages/categories/categories.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -7,6 +8,7 @@ import { AppComponent } from './app.component';
 
 import { InMemoryDatabase } from "./in-memory-database";
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
--- financeiro/src/app/pages/categories/categories.module.ts
+++ financeiro/src/app/pages/categories/categories.module.ts
@@ -4,15 +4,18 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     CategoryListComponent,
-    CategoryFormComponent
+    CategoryFormComponent,
+
   ],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CategoriesModule { }
