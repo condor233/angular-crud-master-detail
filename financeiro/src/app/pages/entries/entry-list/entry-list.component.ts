0616b961ceb5efc972769f68474bdfbd755cd9a1
condor233 <e.hdelemos@gmail.com>
2021-04-16 00:26:15 -0300

    Adiciona entry-list component

---
+++ financeiro/src/app/pages/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,31 @@
+import { EntryService } from './../shared/entry.service';
+import { Component, OnInit } from '@angular/core';
+import { Entry } from '../shared/entry.model';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css']
+})
+export class EntryListComponent implements OnInit {
+
+  entries: Entry[] = [];
+  constructor(private service: EntryService) { }
+
+  ngOnInit() {
+    this.service.getAll().subscribe(
+      entries => this.entries = entries,
+      error => alert('Erro ao carregar a lista')
+    )
+  }
+
+  deleteEntry(entry) {
+    const mustDelete = confirm('Deseja realmente excluir este item? ');
+    if (mustDelete)
+      this.service.delete(entry.id).subscribe(
+        () => this.entries = this.entries.filter(element => element !== entry),
+        () => alert("Erro ao tentar excluit!")
+      )
+  }
+
+}
--- financeiro/src/app/pages/entries/shared/entry.model.ts
+++ financeiro/src/app/pages/entries/shared/entry.model.ts
@@ -2,11 +2,11 @@ import { Category } from './../../categories/shared/category.model';
 export class Entry {
   constructor(
     public id?: number,
-    public name?: String,
-    public description?: String,
-    public type?: String,
-    public amount?: String,
-    public date?: String,
+    public name?: string,
+    public description?: string,
+    public type?: string,
+    public amount?: string,
+    public date?: string,
     public paid?: boolean,
     public categoryId?: number,
     public category?: Category
