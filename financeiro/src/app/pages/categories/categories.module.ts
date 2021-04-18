f89d052aa82a99ffcf82984616d6891f21934275
condor233 <e.hdelemos@gmail.com>
2021-04-18 17:48:10 -0300

    Adiciona reports template

--- financeiro/src/app/pages/categories/categories.module.ts
+++ financeiro/src/app/pages/categories/categories.module.ts
@@ -1,7 +1,5 @@
 import { SharedModule } from '../../shared/shared.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
